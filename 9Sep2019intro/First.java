6ee2eb5d96f2a01346bf9cccbe65586b17b802af
vikrant bhadauria <vikrantbhadauriya2@gmail.com>
2019-09-09 17:46:39 +0530

    Add files via upload

---
+++ 9Sep2019intro/First.java
@@ -0,0 +1,15 @@
+class First{
+
+    public static void main(String args[]){
+
+        int a=2;
+        // float b =4.3;
+        double d =56.67435346;
+        char c='A';
+        String s="i am string";
+        System.out.println("Hello World");
+        System.out.println("The string is "+s);
+
+    }
+
+}
